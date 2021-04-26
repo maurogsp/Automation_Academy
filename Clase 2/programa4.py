d045f1a8e8f9e36b8ab603d3c34f903d2c25111c
maurogsp <maurogaspar@gmail.com>
2021-04-26 18:43:13 -0300

    Subida Tarea Clase 2

--- Clase 2/programa4.py
+++ Clase 2/programa4.py
@@ -5,3 +5,21 @@
 # 3. Al inicio del programa preguntar cuantas personas registrar
 # 4. Hacer que el programa que se hizo se ejecute esas n veces que se pusieron en el punto 3
 # 5. Subir el programa al repositorio creado.
+
+personas = int(input('Ingrese la cantidad de personas que desea registrar: '))
+for i in range (personas):
+    nombre = input('Ingrese su nombre: ')
+    apellido = input('Ingrese su apellido: ')
+    edad = int(input('Ingrese su edad: '))
+    while edad < 1  or edad >120:
+        edad = int(input('Su edad debe ser mayor a 0 y menor o igual a 120. Ingrese su edad nuevamente: '))
+    if edad < 18:
+        condicion = 'menor'
+    elif edad < 65:
+        condicion = 'mayor'
+    elif edad < 120:
+        condicion = 'jubilado'
+    else:
+        condicion = 'cadaver'
+    #print('Su nombre es: '+ nombre+ " " +apellido + ' y usted es '+condicion)
+    print('Su nombre es:',nombre, apellido,'y usted es',condicion)
